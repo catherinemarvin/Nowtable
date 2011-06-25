281981a8e9bea718a287ca355f9a829f02b2c499
Freddie Meyer <dungfu3@gmail.com>
2011-06-25 02:34:27 -0700

    OH SNAP

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 var version = 0.1;
 
-var express = require('express'),
-    sys = require('sys'),
-    formidable = require('formidable');
+var express = require('express');
 
 var server = express.createServer();
+
+server.set('view options', {
+layout: false
+});
+
+var user = {};
+
+server.set('view engine', 'ejs');
 
 // Configuration
 server.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 // Routes
 server.get('/', function(req, res){
-  res.send("lol hi");
+  res.render("index");
 });
 
 server.listen(80);
@@ -19,3 +25,16 @@ console.log("Express server listening on port %d", server.address().port);
 
 var nowjs = require('now');
 var everyone = nowjs.initialize(server);
+
+everyone.now.onclientload = function() {
+	everyone.now.onjoin(this.user.clientId, "");
+	for (var i in user) {
+		this.now.onjoin(i, user[i]);
+	}
+	user[this.user.clientId] = "";
+}
+
+everyone.now.appendtext = function(text) {
+	user[this.user.clientId] += text;
+	everyone.new.refreshtext(this.user.clientId, text);
+}
